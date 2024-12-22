c733a62bac00cb9b17693c1c0c7f77c3eaedd25a
HarukaLuos <serinitimoon23@gmail.com>
2024-12-22 12:10:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const colorData = {
     "cipheris + cipheris": ["cipheris", "mocca", "slate", "shadow", "prune"],
     "cipheris + raisin": ["cipheris", "blackberry", "raisin", "depth", "gunmetal", "seal", "mauve"],
-    "red + blue": ["purple", "violet", "lavender"],  // Пример новой строки
-     "cipheris + ash": ["cipheris", "blackberry", "raisin", "depth", "gunmetal", "seal", "mauve"],
-         "cipheris + vf": ["cipheris", "blackberry", "raisin", "depth", "gunmetal", "seal", "mauve"],
+    "red + blue": ["purple", "violet", "lavender"],
+    "cipheris + ash": ["cipheris", "blackberry", "raisin", "depth", "gunmetal", "seal", "mauve"],
+    "cipheris + vf": ["cipheris", "blackberry", "raisin", "depth", "gunmetal", "seal", "mauve"],
     "cipheris + eggplant": ["cipheris", "blackberry", "raisin", "depth", "wool", "seal", "eggplant"]
 };
 
@@ -13,12 +13,14 @@ function formatString(str) {
 
 function getParentColors() {
     const colorName = formatString(document.getElementById('colorName').value);
+    console.log("Введенное название цвета:", colorName); // Отладочное сообщение
     const resultDiv = document.getElementById('result');
     let resultHTML = '';
     let found = false;
 
     for (let parents in colorData) {
         const childColors = colorData[parents].map(color => color.toLowerCase());
+        console.log("Проверка родительских цветов:", parents, childColors); // Отладочное сообщение
         if (childColors.includes(colorName)) {
             if (!found) {
                 resultHTML += '<h3>Родительские цвета для ' + colorName + ':</h3><ul>';
@@ -35,3 +37,4 @@ function getParentColors() {
         resultDiv.innerHTML = '<p>Нет данных о родительских цветах для ' + colorName + '</p>';
     }
 }
+
